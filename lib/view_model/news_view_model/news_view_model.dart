70dbb1de495eb9bdff54903abc9d7b3845bf6ae5
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-03 23:22:31 +0500

    future method for fetching new data got future uilder

---
+++ lib/view_model/news_view_model/news_view_model.dart
@@ -0,0 +1,18 @@
+
+
+
+import 'package:newsapp/models/news_channel_headlines_model.dart';
+import 'package:newsapp/repository/news_repository/news_repository.dart';
+
+class NewViewModel {
+
+  final _rep = NewsRepository();
+
+
+  Future<NewsChannelsHeadlinesModel> fetachNewsChannelHeadlines(String channelName) async {
+    final responce = await _rep.fetachNewsChannelHeadlines(channelName);
+    return responce;
+  }
+
+
+}
