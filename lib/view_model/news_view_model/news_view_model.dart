25c7d5690e182195d3d0038406cc3638bea0742d
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-04 15:33:45 +0500

    new method is added for categories wise nres

--- lib/view_model/news_view_model/news_view_model.dart
+++ lib/view_model/news_view_model/news_view_model.dart
@@ -1,6 +1,7 @@
 
 
 
+import 'package:newsapp/models/catergories_news_model.dart';
 import 'package:newsapp/models/news_channel_headlines_model.dart';
 import 'package:newsapp/repository/news_repository/news_repository.dart';
 
@@ -11,6 +12,11 @@ class NewViewModel {
 
   Future<NewsChannelsHeadlinesModel> fetachNewsChannelHeadlines(String channelName) async {
     final responce = await _rep.fetachNewsChannelHeadlines(channelName);
+    return responce;
+  }
+
+  Future<CatergoriesNewsModel> fetchCategoriesNewsApi(String channelName) async {
+    final responce = await _rep.fetchCategoriesNewsApi(channelName);
     return responce;
   }
 
